9a5b2bea05a6bae53af21c6e4f2092a037bca033
lenndachen <49460507+lenndachen@users.noreply.github.com>
2019-12-04 16:29:35 -0500

    make content show up on checkout page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import * as ROUTES from "./constants/routes";
 import Home from "./containers/Home";
 import { CartProvider } from "./Context/CartContext.js";
+import Checkout from "./containers/Checkout";
 
 function App() {
   return (
     <CartProvider>
-      <div className="App">
-        <Home />
-      </div>
+      <Router>
+      <Switch>
+            <Route exact path={ROUTES.HOME} component={Home} />
+            <Route exact path={ROUTES.CHECKOUT} component={Checkout} />
+      </Switch>
+      </Router>
     </CartProvider>
   );
 }
--- src/containers/Checkout/index.js
+++ src/containers/Checkout/index.js
@@ -1,2 +1,33 @@
-import React from 'react'
+import React from "react";
+import boom from "../../assets/images/boom.PNG";
+import styles from "../Home/Home.module.css";
+import { CartConsumer } from "../../Context/CartContext";
+import PreviewItem from "../../components/PreviewItem";
 
+class Checkout extends React.Component {
+  render() { 
+     return (
+       <CartConsumer>
+         {props => {
+           console.log(props);
+           return (
+             <div className="checkout-container">
+             <div>
+               <div className={styles["logo"]}>
+                 <img className={styles["boom-logo"]} src={boom} alt="logo" />
+                 </div>
+               </div>
+               <ul>
+                 {props.state.cart.map((cartItem) => { 
+                   return <PreviewItem previewItem={cartItem} />;
+                 })}
+               </ul>
+            </div>
+           );
+         }}
+       </CartConsumer>
+     );
+     }
+    }
+            
+export default Checkout;
